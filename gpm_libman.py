bedbe427e881c8226c5f61e83ee27d6648b89f6f
Conor <obriencjj@gmail.com>
2019-09-23 16:41:17 +0100

    Minor changes. The file is a mess so will be sorted later.

--- gpm_libman.py
+++ gpm_libman.py
@@ -1,30 +1,28 @@
 # https://unofficial-google-music-api.readthedocs.io/en/latest/index.html
 
-# Handles library management
-
 import glob
 import sys
 
 from gmusicapi import Mobileclient
+from gmusicapi import Musicmanager
 
-if len(sys.argv) != 3:
-    print('Specify T/F arguments for uploading and updating playlists')
-    print('e.g. python ' + sys.argv[0] + ' 0 1')
-    print('which would:\n--> not upload new songs\n--> update playlists')
-    sys.exit(0)
+
+def 
 
 #### Parameters
 music_dir = '/home/conor/Music/'
-# If True will upload all new music
-uploading = sys.argv[1]
-# If True will update all playlists
-playlisting = sys.argv[2]
 
 #### Authenticate gpm connection
+
+
 mc = Mobileclient()
 # Line below only done once and token saved to disk
-# mc.perform_oauth
+# mc.perform_oauth()
 mc.oauth_login(mc.FROM_MAC_ADDRESS) # Uses token from above
+
+mm = Musicmanager()
+# mm.perform_oauth()
+
 
 #### Get mp3 file names from music folder
 local_song_names = glob.glob(music_dir + '*.mp3')
@@ -53,3 +51,33 @@ if playlisting:
     pass
 
 #### Add every song to playlists
+
+def main():
+    if len(sys.argv) != 3:
+        print('Specify T/F arguments for uploading and updating playlists')
+        print('e.g. python ' + sys.argv[0] + ' 0 1')
+        print('which would:\n--> not upload new songs\n--> update playlists')
+        sys.exit(0)
+        
+    print('Local music directory set to:', music_dir)
+    accepted = input('Type "y" if this is correct directory: ')
+    
+    if accepted.lower() != 'y':
+        sys.exit(0)
+    
+        
+    # If True will upload all new music
+    uploading = sys.argv[1]
+    if uploading:
+        mc = 
+        
+    # If True will update all playlists
+    playlisting = sys.argv[2]
+    if playlisting:
+        mm.login()
+        
+   
+
+
+if __name__ == '__main__':
+    main()
